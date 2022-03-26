6368e34282ce63a81dc40d4703ce8a3e3b128dec
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-26 17:57:09 +0530

    usereducer in custom hook

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,23 +1,37 @@
-import react,{useState} from "react";
-
-const useInput=(validateFn)=>{
-    const [enteredValue, setEnteredValue] = useState("");
-    const [isTouched, setIsTouched] = useState(false);
-    const valueIsValid = validateFn(enteredValue);
-    const hasError = !valueIsValid && isTouched;
-    const valueChangeHandler = (event) => {
-        setEnteredValue(event.target.value);
-      };
-      const inputBlurHandler = (event) => {
-        setIsTouched(true);
-      };
-      const reset=()=>{
-        setEnteredValue('');
-        setIsTouched(false);
-      };
-    return {
-        enteredValue,valueIsValid,hasError,valueChangeHandler,inputBlurHandler,reset
-    };
-}
+import react, { useState, useReducer } from "react";
+const initialStateObj = {
+  value: "",
+  isTouched: false,
+};
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  } else if (action.type === "BLUR") {
+    return { value: state.value, isTouched: true };
+  } 
+  return initialStateObj;
+};
+const useInput = (validateFn) => {
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialStateObj);
+  const valueIsValid = validateFn(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+  const valueChangeHandler = (event) => {
+    dispatch({ type: "INPUT", value: event.target.value });
+  };
+  const inputBlurHandler = (event) => {
+    dispatch({ type: "BLUR" });
+  };
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+  return {
+    enteredValue: inputState.value,
+    valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 
 export default useInput;
